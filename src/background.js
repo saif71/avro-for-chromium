5198537d5bfedab22897e1e570dfad6b79de610d
saif71 <epbd.saif@gmail.com>
2019-04-02 01:01:30 +0600

    option page now hidden. Due for later work.

--- src/background.js
+++ src/background.js
@@ -1,4 +1,1 @@
-OneSignal.init({appId: "7c151ae1-656d-46b2-adad-b8c664134929",
-                googleProjectNumber: "236753795363"});
-
-//chrome.browserAction.setBadgeText({text: "10+"});
+// //chrome.browserAction.setBadgeText({text: "10+"});
