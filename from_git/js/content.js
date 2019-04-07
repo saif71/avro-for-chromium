25a8bf01c0202313c8f78606ef78496d54abbd82
Salman Hossain Saif <epbd.saif@gmail.com>
2019-04-07 20:05:37 +0600

    initial

---
+++ from_git/js/content.js
@@ -0,0 +1,78 @@
+/*
+	=============================================================================
+	*****************************************************************************
+	The contents of this file are subject to the Mozilla Public License
+	Version 1.1 (the "License"); you may not use this file except in
+	compliance with the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+
+	Software distributed under the License is distributed on an "AS IS"
+	basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+	License for the specific language governing rights and limitations
+	under the License.
+
+	The Original Code is Avro Keyboard for Google Chrome
+
+	The Initial Developer of the Original Code is
+	Rifat Nabi <to.rifat@gmail.com>
+
+	Copyright (C) OmicronLab (http://www.omicronlab.com). All Rights Reserved.
+
+
+	Contributor(s): ______________________________________.
+
+	*****************************************************************************
+	=============================================================================
+*/
+/*global jQuery, chrome, console, document, window, noty, localStorage */
+(function ($) {
+    'use strict';
+    
+    function sendStatus(bangla) {
+        chrome.runtime.sendMessage({method: 'handleStatus', status: bangla});
+    }
+
+    var selector = 'textarea, input[type=text],input[type=search]';
+    $.noConflict();
+    
+    chrome.runtime.sendMessage({method: 'hotKey'}, function(response) {
+        $(function () {
+            var callback = function (isBangla) {
+                sendStatus(isBangla);
+            };
+
+            $(selector).avro({bangla: false, hotkey: response.hotkey }, callback);
+            $(window).on('DOMNodeInserted', function () {
+                $(selector).avro({bangla: false, hotkey: response.hotkey }, callback);
+            });
+
+            $(selector).on('blur', function () {
+                sendStatus(false);
+            });
+        });
+    });
+    
+    if(typeof localStorage.AvroKeyboard === 'undefined') {
+        localStorage.AvroKeyboard = true;
+        chrome.runtime.sendMessage({method: 'popupCount'}, function(response) {
+            if (response.popupCount <= 3) {
+                $(window).load(function(){
+                    noty({
+                        text: "Avro Keyboard - Press <big>Ctrl+M</big> to switch keyboard",
+                        layout: "top",
+                        type: "success",
+                        textAlign: "center",
+                        easing: "swing",
+                        animateOpen: {"height":"toggle"},
+                        animateClose: {"height":"toggle"},
+                        speed: 500,
+                        timeout: 10000,
+                        closable: true,
+                        closeOnSelfClick: true
+                    });
+                });
+            }
+        });
+    }
+
+}(jQuery));
---
+++ from_git/js/options.js
@@ -0,0 +1,57 @@
+function save_options() {
+    var select = document.getElementById("hotkey");
+    var hotkey = select.children[select.selectedIndex].value;
+    localStorage["hotkey"] = hotkey;
+    
+    noty({
+        text: "Setting Saved",
+        layout: "top",
+        type: "success",
+        textAlign: "center",
+        easing: "swing",
+        animateOpen: {"height":"toggle"},
+        animateClose: {"height":"toggle"},
+        speed: 500,
+        timeout: 10000,
+        closable: true,
+        closeOnSelfClick: true
+    });
+    
+}
+
+function addOption(key,keyName){
+            $("<option/>", {
+            value: key,
+            html: keyName
+        }).appendTo("#hotkey");
+}
+
+function populate_options(){
+    //Alphabets
+    for (i = 65 ; i <= 90 ; ++i){
+        addOption(i, String.fromCharCode(i));
+    }
+    
+    //Space key
+    addOption(32,"Space");
+    addOption(190," . ");
+
+    restore_options();
+}
+
+function restore_options() {
+    var hotkey = localStorage["hotkey"];
+    if (!hotkey) {
+        return;
+    }
+    var select = document.getElementById("hotkey");
+    for (var i = 0; i < select.children.length; i++) {
+        var child = select.children[i];
+        if (child.value == hotkey) {
+            child.selected = "true";
+            break;
+        }
+    }
+}
+document.addEventListener('DOMContentLoaded', populate_options);
+document.querySelector('#save').addEventListener('click', save_options);
