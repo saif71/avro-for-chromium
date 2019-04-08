b6b515721f7b6fd436ea0ab498b7068f80761818
Salman Hossain Saif <epbd.saif@gmail.com>
2019-04-08 14:17:42 +0600

    working on textarea with bn enabled by default

--- src/background.js
+++ src/background.js
@@ -1,1 +1,12 @@
 // //chrome.browserAction.setBadgeText({text: "10+"});
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.greeting == "bn_enable"){
+        sendResponse({farewell: "Bangla Enabled"});
+        chrome.browserAction.setBadgeText({text: "BN"});}
+
+    else{
+        sendResponse({farewell: "English Enabled"});
+        chrome.browserAction.setBadgeText({text: ""});
+    }
+    });
